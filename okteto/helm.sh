07e4a39c0b45e4cbe7c45f723accc63eaaf5b468
takokun <takokun.778@gmail.com>
2023-03-18 23:18:14 +0900

    chore(#76) helmコマンドを修正

---
+++ okteto/helm.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -eu
+
+helm upgrade --install --wait morning-night-dream-platform-secret ./charts/secret --namespace=morning-night-dreamer
+
+helm upgrade --install --wait morning-night-dream-platform-database ./charts/database --namespace=morning-night-dreamer
+
+helm upgrade --install --wait morning-night-guild-platform-app-core ./charts/app-core --namespace=morning-night-dreamer
+
+helm upgrade --install --wait morning-night-guild-platform-app-api ./charts/app-api --namespace=morning-night-dreamer
